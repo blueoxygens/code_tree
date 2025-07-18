9a31ad46daf13f4cafa37dbb76e0c230b518cce5
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-07-18 17:49:53 +0900

    Title: 최대로 겹치는 구간, Time: 289ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250718/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 250718/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -1,33 +1,35 @@
 import java.util.Scanner;
 import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] x1 = new int[n];
-        int[] x2 = new int[n];
+
+        int[] starts = new int[n];
+        int[] ends = new int[n];
+
         for (int i = 0; i < n; i++) {
-            x1[i] = sc.nextInt();
-            x2[i] = sc.nextInt();
-        }
-        // Please write your code here.
-        int max = 0;
-        int[] range = new int [2];
-        range[0] = Arrays.stream(x1).min().getAsInt();
-        range[1] = Arrays.stream(x2).max().getAsInt();
-        
-        for(int i =range[0]; i<=range[1]; i++){
-            int count = 0;
-            for(int j=0; j<n; j++){
-                if(x1[j]<=i && x2[j]>=i || x1[j]>=i && x2[j]<=i){
-                    count += 1;
-                }
-            }
-            if(max<count){
-                max = count;
-            }
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            starts[i] = Math.min(a, b); // 항상 작은 값이 시작
+            ends[i] = Math.max(a, b);   // 큰 값이 끝
         }
 
-        System.out.println(max);
+        int minPoint = Arrays.stream(starts).min().getAsInt();
+        int maxPoint = Arrays.stream(ends).max().getAsInt();
+        int maxOverlap = 0;
+
+        for (int i = minPoint; i <= maxPoint; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (starts[j] <= i && i <= ends[j]) {
+                    count++;
+                }
+            }
+            maxOverlap = Math.max(maxOverlap, count);
+        }
+
+        System.out.println(maxOverlap);
     }
 }
