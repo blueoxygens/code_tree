7b38b3099aad05fc34d7c01bc51dadf71baf45d7
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-07-19 18:03:35 +0900

    Title: 왔다 갔던 구역 2, Time: 127ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 250719/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 250719/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -1,39 +1,43 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int [] field = new int [200];
+        int[] field = new int[200];
         int pivot = 99;
+
         for (int i = 0; i < N; i++) {
             int x = sc.nextInt();
             char dir = sc.next().charAt(0);
-            // Please write your code here.
-            if (dir == 'R'){
-                for(int j = pivot; j <= pivot + x; j ++){
-                    field[j] += 1;
+
+            if (dir == 'R') {
+                for (int j = pivot; j < pivot + x; j++) {
+                    field[j]++;
                 }
                 pivot += x;
-            }else{
-                for(int j = pivot; j >= pivot - x; j --){
-                    field[j] += 1;
+            } else {
+                for (int j = pivot - 1; j >= pivot - x; j--) {
+                    field[j]++;
                 }
                 pivot -= x;
             }
         }
+
         int ans = 0;
-        for(int i = 0; i < 200; i++){
-            if(field[i]>=2){
+        for (int i = 0; i < 200; i++) {
+            if (field[i] >= 2) {
                 int len = 0;
                 int temp = i;
-                while(field[temp] >=2){
-                    len += 1;
-                    ++ temp;
+                while (temp < 200 && field[temp] >= 2) {
+                    len++;
+                    temp++;
                 }
-                ans += len -1;
-                i = ++ temp;
+                ans += len;
+                i = temp - 1;
             }
         }
+
         System.out.println(ans);
     }
 }
