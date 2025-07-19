947cda064590877193d57c9d92657039e2b00aa0
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-07-19 17:39:49 +0900

    Title: 왔다 갔던 구역 2, Time: 115ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250719/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int [] field = new int [200];
+        int pivot = 99;
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            char dir = sc.next().charAt(0);
+            // Please write your code here.
+            if (dir == 'R'){
+                for(int j = pivot; j <= pivot + x; j ++){
+                    field[j] += 1;
+                }
+                pivot += x;
+            }else{
+                for(int j = pivot; j >= pivot - x; j --){
+                    field[j] += 1;
+                }
+                pivot -= x;
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < 200; i++){
+            if(field[i]>=2){
+                int len = 0;
+                int temp = i;
+                while(field[temp] >=2){
+                    len += 1;
+                    temp++;
+                }
+                ans += len -1;
+                i = temp;
+            }
+        }
+        System.out.println(ans);
+    }
+}
