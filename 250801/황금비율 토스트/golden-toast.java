b2014f7087eb09e89d8d707c80c248e00c3af19f
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-08-01 16:21:58 +0900

    Title: 황금비율 토스트, Time: 174ms, Memory: 12MB, Status: Runtime Error - Codetree

---
+++ 250801/황금비율 토스트/golden-toast.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        char[] s = sc.next().trim().toCharArray();
+        sc.nextLine();
+        LinkedList<Character> list = new LinkedList<>();
+        for(char c : s){
+            list.add(c);
+        }
+
+        ListIterator<Character> it = list.listIterator(list.size());
+
+        for (int i = 0; i < m; i++) {
+            String[] command = sc.nextLine().trim().split(" ");
+            switch (command[0]){
+                case "L":
+                    if(it.hasPrevious()){
+                        it.previous();
+                    }
+                    break;
+                case "P":
+                    it.add(command[1].charAt(0));
+                    // System.out.println(command[1].charAt(0));
+                    break;
+                case "R":
+                    if(it.hasNext()){
+                        it.next();
+                    }
+                    break;
+                case "D":
+                    it.next();
+                    it.remove();
+                    break;
+                default:
+                    break;
+            }
+        }
+        it = list.listIterator();
+        while(it.hasNext()){
+            System.out.print(it.next());
+        }
+        
+        // Please write your code here.
+
+        //L 앞으로, R 뒤로, 뒤에 있는 요소 삭제, P &는 &라는 문자가 적혀있는 식빵을 추가 
+        
+    }
+}
