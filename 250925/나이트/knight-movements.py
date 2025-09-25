8f2f8529fb91b3ee576da428de8497e1b2b01de2
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-09-25 23:43:08 +0900

    Title: 나이트, Time: 1099ms, Memory: 74MB, Status: Time Limit Exceed - Codetree

---
+++ 250925/나이트/knight-movements.py
@@ -0,0 +1,30 @@
+from collections import deque
+import math
+
+n = int(input())
+r1, c1, r2, c2 = map(int, input().split())
+
+# Please write your code here.
+memo = [[math.inf]*(n+1) for _ in range(n+1)]
+memo[r2][c2] = 0
+
+def bfs():
+    directions = [(-2,-1),(-2,1),(2,1),(2,-1),(-1,-2),(-1,2),(1,-2),(1,2)]
+    visited = set()
+    q = deque()
+    q.append((r2,c2))
+    while q:
+        r, c = q.popleft()
+        for dr, dc in directions:
+            nr, nc = r + dr, c + dc
+            if 1<= nr <= n and 1 <= nc <= n and (nr,nc) not in visited :
+                q.append((nr,nc))
+                memo[nr][nc] = min(memo[r][c] + 1, memo[nr][nc])
+        visited.add((r,c))
+
+bfs()
+
+if memo[r1][c1] == math.inf:
+    print(-1)
+else:
+    print(memo[r1][c1])
