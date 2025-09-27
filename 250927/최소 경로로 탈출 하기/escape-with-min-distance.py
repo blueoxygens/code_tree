d335269b771ae6887c7f3b36144aa3fbc972c614
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-09-27 17:08:44 +0900

    Title: 최소 경로로 탈출 하기, Time: 87ms, Memory: 18MB, Status: Passed - Codetree

--- 250927/최소 경로로 탈출 하기/escape-with-min-distance.py
+++ 250927/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -3,27 +3,25 @@ from collections import deque
 n, m = map(int, input().split())
 a = [list(map(int, input().split())) for _ in range(n)]
 
-# Please write your code here.
-
 d = [(1,0),(0,-1),(-1,0),(0,1)]
 
 def bfs():
     q = deque()
     q.append((0, 0, 0))
-
+    
+    # set 대신 2차원 visited 배열 사용
     visited = [[False]*m for _ in range(n)]
     visited[0][0] = True
 
     while q:
         y, x, lv = q.popleft()
         for dy, dx in d:
-            ny = y + dy
-            nx = x + dx
+            ny, nx = y + dy, x + dx
             if 0 <= ny < n and 0 <= nx < m and not visited[ny][nx] and a[ny][nx] != 0:
                 if ny == n-1 and nx == m-1:
                     return lv+1
-                q.append((ny,nx, lv+1))
-                visited[y][x] = True
+                visited[ny][nx] = True  # 큐에 넣을 때 바로 방문 체크
+                q.append((ny, nx, lv+1))
     return -1
 
-print(bfs())       
+print(bfs())
