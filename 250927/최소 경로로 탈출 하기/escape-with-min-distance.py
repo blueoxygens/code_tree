5f16d4114e98a5624badf578376960313f14045d
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-09-27 16:56:02 +0900

    Title: 최소 경로로 탈출 하기, Time: 693ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 250927/최소 경로로 탈출 하기/escape-with-min-distance.py
+++ 250927/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -9,7 +9,7 @@ d = [(1,0),(0,-1),(-1,0),(0,1)]
 
 def bfs():
     q = deque()
-    q.append((0,0,0))
+    q.append((n-1, m-1, 0))
     visited = set()
     while q:
         y, x, lv = q.popleft()
@@ -17,7 +17,7 @@ def bfs():
             ny = y + dy
             nx = x + dx
             if 0 <= ny < n and 0 <= nx < m and (ny,nx) not in visited and a[ny][nx] != 0:
-                if ny == n-1 and nx == m-1:
+                if ny == 0 and nx == 0:
                     return lv+1
                 q.append((ny,nx, lv+1))
         visited.add((y,x))
