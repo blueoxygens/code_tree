078b182f7265f5532ec262ef9de8899c059c887b
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-09-30 16:34:54 +0900

    Title: 체크판위에서 2, Time: 54ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250930/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,20 @@
+from collections import deque
+R, C = map(int, input().split())
+grid = [list(input().split()) for _ in range(R)]
+
+# Please write your code here.
+start = grid[0][0]
+count = 0
+fq = deque()
+for i in range (1,R-1):
+    for j in range(1,C-1):
+        if grid[i][j] != start:
+            fq.append((i,j))
+while fq:
+    y, x = fq.popleft()
+    
+    for i in range(y+1,R-1):
+        for j in range(x+1,C-1):
+            if grid[i][j] == start:
+                count += 1
+print(count)
