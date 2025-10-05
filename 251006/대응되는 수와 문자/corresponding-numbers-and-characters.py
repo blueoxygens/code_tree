0bbe733d55b3a436c4edf8b59b6d727479c22819
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-10-06 00:31:29 +0900

    Title: 대응되는 수와 문자, Time: 234ms, Memory: 37MB, Status: Passed - Codetree

--- 251006/대응되는 수와 문자/corresponding-numbers-and-characters.py
+++ 251006/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -5,11 +5,9 @@ words = [""] + [input() for _ in range(n)]
 queries = [input() for _ in range(m)]
 
 # Please write your code here.
+d = {v:i for i, v in enumerate(words)}
 for q in queries:
     if q.isdigit():
         print(words[int(q)])
     else:
-        for i, v in enumerate(words):
-            if v == q:
-                print(i)
-                break
+        print(d[q])
