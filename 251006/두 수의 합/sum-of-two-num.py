5f51ccaceca3cb9b18aa60560cf3ec135111a2d8
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-10-06 00:58:48 +0900

    Title: 두 수의 합, Time: 85ms, Memory: 26MB, Status: Passed - Codetree

--- 251006/두 수의 합/sum-of-two-num.py
+++ 251006/두 수의 합/sum-of-two-num.py
@@ -1,19 +1,15 @@
 from collections import Counter
+
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
-# Please write your code here.
-#Backtracking으로 할까 걍
-ans = []
-def p():
-    def backtracking(start, a):
-        if len(a) == 2:
-            if sum(a) == k:
-                ans.append(tuple(a))
-        for i in range(start, n-1):
-            a.append(arr[i])
-            b = backtracking(i+1, a)
-            a.pop()
-    backtracking(0,[])
-p()
-print(len(ans))
+count = Counter()
+pairs = 0
+
+for num in arr:
+    complement = k - num
+    if count[complement]:
+        pairs += count[complement]
+    count[num] += 1
+
+print(pairs)
