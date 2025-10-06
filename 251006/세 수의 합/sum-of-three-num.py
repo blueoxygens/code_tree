e7a66eba64349af7ed4e75ed66dce50cde2fcd8b
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-10-06 10:44:38 +0900

    Title: 세 수의 합, Time: 1091ms, Memory: 20MB, Status: Time Limit Exceed - Codetree

--- 251006/세 수의 합/sum-of-three-num.py
+++ 251006/세 수의 합/sum-of-three-num.py
@@ -1,25 +1,17 @@
+from itertools import combinations
+
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
 #이건 진짜 backtracking이다
 
-def find_pairs():
-    ans = []
-    def bt (start, picked):
-        if len(picked) == 3:
-            if sum(picked) == k:
-                ans.append(tuple(picked))
-            return
-        
-        for i in range(start, n):
-            picked.append(arr[i])
-            # print(picked)
-            bt(i+1, picked)
-            picked.pop()
-    
-    bt(0, [])
-    # print(ans)
-    return len(ans)
+ans = combinations(arr,3)
 
-print(find_pairs())
+count = 0
+
+for a in ans:
+    if sum(a) == k:
+        count += 1
+
+print(count)
