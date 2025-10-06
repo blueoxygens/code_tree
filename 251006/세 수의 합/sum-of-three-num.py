242912053b5a9dc2514148486821f8c424c215fc
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-10-06 11:43:02 +0900

    Title: 세 수의 합, Time: 73ms, Memory: 18MB, Status: Passed - Codetree

--- 251006/세 수의 합/sum-of-three-num.py
+++ 251006/세 수의 합/sum-of-three-num.py
@@ -1,14 +1,15 @@
+from collections import Counter
+
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 cnt = 0
 for i in range(n - 2):
-    seen = set()
+    seen = Counter()
     target = k - arr[i]
     for j in range(i + 1, n):
         complement = target - arr[j]
-        if complement in seen:
-            cnt += 1
-        seen.add(arr[j])
+        cnt += seen[complement]
+        seen[arr[j]] += 1
 
 print(cnt)
