e21a4f95a3867af017d42ddd57598dfda5538945
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-10-06 11:10:47 +0900

    Title: 세 수의 합, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251006/세 수의 합/sum-of-three-num.py
+++ 251006/세 수의 합/sum-of-three-num.py
@@ -1,17 +1,14 @@
-from itertools import combinations
-
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
-# Please write your code here.
-#이건 진짜 backtracking이다
+cnt = 0
+for i in range(n - 2):
+    seen = set()
+    target = k - arr[i]
+    for j in range(i + 1, n):
+        complement = target - arr[j]
+        if complement in seen:
+            cnt += 1
+        seen.add(arr[j])
 
-ans = combinations(arr,3)
-
-count = 0
-
-for a in ans:
-    if sum(a) == k:
-        count += 1
-
-print(count)
+print(cnt)
