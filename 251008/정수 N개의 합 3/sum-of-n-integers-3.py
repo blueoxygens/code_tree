ddb1bb5913a1ee93129f2321404b34d9da480964
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-10-08 18:57:39 +0900

    Title: 정수 N개의 합 3, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251008/정수 N개의 합 3/sum-of-n-integers-3.py
@@ -0,0 +1,18 @@
+n, k = map(int, input().split())
+arr = [[0]*(n+1)]
+for _ in range(n):
+    arr.append([0] + list(map(int, input().split())))
+# Please write your code here.
+ps = [[0]*(n+1) for _ in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        ps[i][j] = ps[i-1][j] + ps[i][j-1] - ps[i-1][j-1] + arr[i][j]
+
+# 정사각형 탐색 진행하는데..k * k 크기만큼 진행
+for i in ps:
+    print(' '.join(map(str, i)))
+ans = 0
+for i in range(k,n+1):
+    for j in range(k,n+1):
+        ans = max(ans, ps[i][j] - ps[i-1][j] - ps[i][j-1] + arr[1][1])
+print(ans)
