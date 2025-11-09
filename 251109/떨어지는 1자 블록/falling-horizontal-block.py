a37ca48f09b2fd37a59db7b31c17c26c869a72d4
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-11-09 16:16:40 +0900

    Title: 떨어지는 1자 블록, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251109/떨어지는 1자 블록/falling-horizontal-block.py
+++ 251109/떨어지는 1자 블록/falling-horizontal-block.py
@@ -2,16 +2,20 @@ n, m, k = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-for i in range (n-1):
-    for j in range (k-1, k+m-1): # -> 여기가 이상함
-        # print(f'case: {i+1} {j}')
-        if grid[i+1][j] == 1:
-            for j in range(k-1, k+m-1):
-                grid[i][j] = 1
-            break
-    else:
-        continue
-    break
 
+if n > 1:
+    for i in range (n-1):
+        for j in range (k-1, k+m-1): # -> 여기가 이상함
+        # print(f'case: {i+1} {j}')
+            if grid[i+1][j] == 1:
+                for j in range(k-1, k+m-1):
+                    grid[i][j] = 1
+                break
+        else:
+            continue
+        break
+else:
+    for j in range (k-1, k+m-1):
+        grid[0][j] = 1
 for i in grid:
     print(*i)
