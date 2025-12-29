935c62c33ac327ed50bcdef007f0c00eb5cc02c0
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2025-12-29 21:28:45 +0900

    Title: 겹치지 않게 선분 고르기, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 251229/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 251229/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,15 +12,26 @@ public class Main {
         // Please write your code here.
         int max = 0;
         for(int i = 0; i<n; i++){
-            HashSet<Integer> set = new HashSet<Integer>();
+            int [] check = new int[1001];
             int count = 1;
-            set.add(segments[i][0]);
-            set.add(segments[i][1]);
-            for(int j=i+1; j<n; j++){
-                if(!set.contains(segments[j][0]) && !set.contains(segments[j][1])){
+            for(int j = segments[i][0]; j<=segments[i][1]; j++){
+                check[j] = 1;
+            }
+            for(int j= i+1; j<n; j++){
+                boolean flag = false;
+                for(int k = segments[j][0]; k<=segments[j][1]; k++){
+                    if (check[k] == 1){
+                        flag = true;
+                        break;
+                    }
+                }
+                if(flag){
+                    continue;
+                } else{
                     count += 1;
-                    set.add(segments[j][0]);
-                    set.add(segments[j][1]);
+                    for(int k = segments[j][0]; k<=segments[j][1]; k++){
+                        check[k] = 1;
+                    }
                 }
             }
             max = Math.max(max, count);
