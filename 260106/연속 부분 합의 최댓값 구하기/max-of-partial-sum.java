9fef95612ffb44f0f3ae07a5842c283aee24732e
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2026-01-06 15:32:40 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260106/연속 부분 합의 최댓값 구하기/max-of-partial-sum.java
+++ 260106/연속 부분 합의 최댓값 구하기/max-of-partial-sum.java
@@ -1,5 +1,4 @@
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -9,29 +8,11 @@ public class Main {
             nums[i] = sc.nextInt();
         }
         // Please write your code here.
-        int[][] dp = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], Integer.MIN_VALUE);
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        for(int i = 1; i<n; i++){
+            dp[i] = Math.max(dp[i-1]+nums[i], nums[i]);
         }
-        for(int i =0; i<n; i++)
-            dp[i][i] = nums[i];
-        for(int i = 0; i<n-1; i++){
-            for(int j = i+1; j<n; j++){
-                dp[i][j] = dp[i][j-1] + nums[j];
-            }
-        }
-
-        int max = Arrays.stream(dp)
-            .flatMapToInt(Arrays::stream)
-            .max()
-            .orElse(-1);
-
-        // String result = Arrays.stream(dp)
-        //     .map(row -> Arrays.stream(row)
-        //                     .mapToObj(String::valueOf)
-        //                     .collect(Collectors.joining(" ")))
-        //     .collect(Collectors.joining("\n"));
-
-        System.out.println(max);
+        System.out.println(dp[n-1]);
     }
 }
