a9e66e5ca153662938c60bf2be4667627be9266e
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2026-01-17 19:23:03 +0900

    Title: 최장 공통 부분 수열, Time: 103ms, Memory: 25MB, Status: Passed - Codetree

--- 260117/최장 공통 부분 수열/longest-common-sequence.py
+++ 260117/최장 공통 부분 수열/longest-common-sequence.py
@@ -4,6 +4,15 @@ A = input()
 B = input()
 
 # Please write your code here.
-ac =Counter(A)
-bc =Counter(B)
-print(ac.keys())
+A = list(A)
+B = list(B)
+dp = [[0]*(len(A)+1) for _ in range((len(B))+1)]
+
+for i in range(len(B)):
+    for j in range(len(A)):
+        if A[j] == B[i]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+
+print(dp[len(B)-1][len(A)-1])
