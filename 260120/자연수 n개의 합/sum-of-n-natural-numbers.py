f0cb6895c7a51246e5ffa867905b61230c1e8f94
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2026-01-20 19:53:18 +0900

    Title: 자연수 n개의 합, Time: 1097ms, Memory: 16MB, Status: Time Limit Exceed - Codetree

--- 260120/자연수 n개의 합/sum-of-n-natural-numbers.py
+++ 260120/자연수 n개의 합/sum-of-n-natural-numbers.py
@@ -1,11 +1,11 @@
 s = int(input())
 
 # Please write your code here.
-sum = [0]*(s+1)
+sum = 0
 idx = 0
 for i in range(1,s):
-    sum[i] = sum[i-1] + i
-    if sum[i] > s:
+    sum += i
+    if sum > s:
         idx = i-1
         break
 print(idx)
