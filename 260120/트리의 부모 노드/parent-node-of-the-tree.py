3bb4ea62c241a8b7699f4cec5f96bcb91117baf5
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2026-01-20 21:43:19 +0900

    Title: 트리의 부모 노드, Time: 355ms, Memory: 100MB, Status: Memory Limit Exceed - Codetree

--- 260120/트리의 부모 노드/parent-node-of-the-tree.py
+++ 260120/트리의 부모 노드/parent-node-of-the-tree.py
@@ -1,9 +1,26 @@
+from collections import deque
 n = int(input())
 edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
 
 # Please write your code here.
-edges = sorted(edges, key=lambda x: x[1])
-# print(edges)
+edge = [[-1]*(n+1) for _ in range(n+1)]
+for n1, n2 in edges:
+    edge[n1][n2], edge[n2][n1] = 1, 1
 
-for p, c in edges:
-    print(p)
+p = {}
+
+q = deque()
+visited = set()
+q.append(1)
+visited.add(1)
+
+while q:
+    t = q.popleft()
+    for i in range(1,n+1):
+        if i not in visited and edge[t][i] == 1:
+            p[i] = t
+            q.append(i)
+            visited.add(i)
+
+for i in range(2, n+1):
+    print(p[i])
