74ae628b81ec179db238f338b3615a78f0ace32e
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2026-02-12 22:42:39 +0900

    Title: 야바위, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260212/야바위/ya-rock.py
@@ -0,0 +1,33 @@
+n = int(input())
+moves = [tuple(map(int, input().split())) for _ in range(n)]
+a, b, c = zip(*moves)
+a, b, c = list(a), list(b), list(c)
+
+# Please write your code here.
+dic = {1:[],2:[],3:[]}
+score = [0]*4
+
+def mix(key, a, b, c):
+    if dic[key]:
+        t = dic[key].pop()
+    else:
+        t = key
+
+    if t != a and t == b:
+        dic[key].append(a)
+        if a == c:
+            score[key] += 1
+    elif t == a:
+        dic[key].append(b)
+        if b == c:
+            score[key] += 1
+    else:
+        if t == c:
+            score[key] += 1
+
+
+for i in range(n):
+    for k in list(dic.keys()):
+        mix(k, a[i], b[i], c[i])
+
+print(max(score))
