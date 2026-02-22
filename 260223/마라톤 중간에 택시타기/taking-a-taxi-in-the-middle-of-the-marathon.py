c33121daca0d0c3e21a1b3513d8df4965dd240db
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2026-02-23 00:05:03 +0900

    Title: 마라톤 중간에 택시타기, Time: 78ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260223/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.py
@@ -0,0 +1,20 @@
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+x = [p[0] for p in points]
+y = [p[1] for p in points]
+
+# Please write your code here.
+def distance (x,y,u,v):
+    return abs(x-u)+abs(y-v)
+
+r,l = [0]*n,[0]*n
+
+for i in range(1,n):
+    l[i] = l[i-1] + distance(x[i],y[i],x[i-1],y[i-1])
+    r[n-i-1] = r[n-i] + distance(x[n-i],y[n-i],x[n-i-1],y[n-i-1])
+
+ans = 10000
+for i in range(1,n-1):
+    ans = min(ans, l[i-1] + r[i+1] + distance(x[i-1],y[i-1],x[i+1],y[i+1]))
+
+print(ans)
