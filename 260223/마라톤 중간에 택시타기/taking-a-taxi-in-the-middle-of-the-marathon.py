ea3d8edb142f157b2789cd6c98f8bf534dd6124b
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2026-02-23 00:05:40 +0900

    Title: 마라톤 중간에 택시타기, Time: 286ms, Memory: 33MB, Status: Passed - Codetree

--- 260223/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.py
+++ 260223/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.py
@@ -1,3 +1,4 @@
+import math
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
 x = [p[0] for p in points]
@@ -13,7 +14,7 @@ for i in range(1,n):
     l[i] = l[i-1] + distance(x[i],y[i],x[i-1],y[i-1])
     r[n-i-1] = r[n-i] + distance(x[n-i],y[n-i],x[n-i-1],y[n-i-1])
 
-ans = 10000
+ans = math.inf
 for i in range(1,n-1):
     ans = min(ans, l[i-1] + r[i+1] + distance(x[i-1],y[i-1],x[i+1],y[i+1]))
 
