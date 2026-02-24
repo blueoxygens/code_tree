72317c2b7e901bdfe315b1377e5390f5855f81c1
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2026-02-24 23:55:24 +0900

    Title: 가장 많이 겹치는 구간, Time: 111ms, Memory: 21MB, Status: Passed - Codetree

--- 260224/가장 많이 겹치는 구간/section-with-maximum-overlap.py
+++ 260224/가장 많이 겹치는 구간/section-with-maximum-overlap.py
@@ -2,12 +2,13 @@ n = int(input())
 intervals = [tuple(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-rec = {}
+rintervals = sorted(intervals, key=lambda x:x[1])
+l = rintervals[n-1][1]
+memo = [0]*(l+1)
+start = 0
 for x1, x2 in intervals:
-    for i in range(x1,x2+1):
-        if i not in rec:
-            rec[i] = 1
-        else:
-            rec[i] += 1
-
-print(max(list(rec.values())))
+    memo[x1] = 1
+    memo[x2] = -1
+for i in range(1,l+1):
+    memo[i] += memo[i-1]
+print(max(memo))
