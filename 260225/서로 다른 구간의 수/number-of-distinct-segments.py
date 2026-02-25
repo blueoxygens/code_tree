e81bc50f436ab8cc09d1423f2c5c027bc340ab8e
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2026-02-25 13:44:57 +0900

    Title: 서로 다른 구간의 수, Time: 214ms, Memory: 25MB, Status: Wrong Answer - Codetree

--- 260225/서로 다른 구간의 수/number-of-distinct-segments.py
+++ 260225/서로 다른 구간의 수/number-of-distinct-segments.py
@@ -4,25 +4,13 @@ intervals = [tuple(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 rintervals =  sorted(intervals, key=lambda x: x[1])
-l = rintervals[n-1][1]
-memo = [0]*(l+2)
-for a, b in intervals:
-    memo[a] = 1
-    memo[b] = -1
-
-q = deque(memo)
-
+end = 0
 ans = 0
-stck = deque()
-while q:
-    t = q.popleft()
-    if t == 1:
-        stck.append(t)
-    elif t == -1:
-        stck.pop()
-        while stck:
-            if q.popleft() == -1:
-                stck.pop()
+for a, b in rintervals:
+    if a > end:
         ans += 1
+        end  = b
+    else:
+        end = b
 
 print(ans)
