ca21b777cbcb505c4d25b4a1bf34161176c7e02d
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2026-02-27 18:09:13 +0900

    Title: 삼 오 무, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 260227/삼 오 무/three-five-moo.py
+++ 260227/삼 오 무/three-five-moo.py
@@ -1,19 +1,12 @@
 n = int(input())
 
-# Please write your code here.
-# 8 -> 15당
+i = (n - 1) // 8  # n=8일 때 i=0이 아닌 올바른 주기 계산
+count = i * 8
 
-i = n//8
-
-count = i*8
-ans = 0
-
-for j in range(i*15+1,i*15+16):
-    if j % 3 ==0 or j % 5 ==0:
+for j in range(i * 15 + 1, i * 15 + 16):
+    if j % 3 == 0 or j % 5 == 0:
         continue
     count += 1
     if count == n:
-        ans = j
+        print(j)
         break
-
-print(ans)
