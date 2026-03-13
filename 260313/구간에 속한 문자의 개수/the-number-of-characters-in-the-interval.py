55dc81efb206b6654e1560d47e68cffb21bbd7e2
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2026-03-13 20:49:39 +0900

    Title: 구간에 속한 문자의 개수, Time: 375ms, Memory: 40MB, Status: Passed - Codetree

--- 260313/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
+++ 260313/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
@@ -7,9 +7,25 @@ grid = [input() for _ in range(n)]
 queries = [tuple(map(int, input().split())) for _ in range(k)]
 
 # Please write your code here.
-cgrid = [(0,0,0)*(m+1) for _ in range(n+1)]
+agrid = [[0]*(m+1) for _ in range(n+1)]
+bgrid = [[0]*(m+1) for _ in range(n+1)]
+cgrid = [[0]*(m+1) for _ in range(n+1)]
 
 for i in range(n):
     for j in range(m):
         t = grid[i][j]
-        cgrid[i+1][j+1] = 
+        if t == 'a':
+            agrid[i+1][j+1] = agrid[i][j+1] + agrid[i+1][j] - agrid[i][j] + 1
+            bgrid[i+1][j+1] = bgrid[i][j+1] + bgrid[i+1][j] - bgrid[i][j]
+            cgrid[i+1][j+1] = cgrid[i][j+1] + cgrid[i+1][j] - cgrid[i][j]
+        elif t == 'b':
+            agrid[i+1][j+1] = agrid[i][j+1] + agrid[i+1][j] - agrid[i][j]
+            bgrid[i+1][j+1] = bgrid[i][j+1] + bgrid[i+1][j] - bgrid[i][j] + 1
+            cgrid[i+1][j+1] = cgrid[i][j+1] + cgrid[i+1][j] - cgrid[i][j]
+        else:
+            agrid[i+1][j+1] = agrid[i][j+1] + agrid[i+1][j] - agrid[i][j]
+            bgrid[i+1][j+1] = bgrid[i][j+1] + bgrid[i+1][j] - bgrid[i][j]
+            cgrid[i+1][j+1] = cgrid[i][j+1] + cgrid[i+1][j] - cgrid[i][j] + 1
+
+for a, b, c, d in queries:
+    print(f'{agrid[c][d] - agrid[a-1][d] - agrid[c][b-1] + agrid[a-1][b-1]} {bgrid[c][d] - bgrid[a-1][d] - bgrid[c][b-1] + bgrid[a-1][b-1]} {cgrid[c][d] - cgrid[a-1][d] - cgrid[c][b-1] + cgrid[a-1][b-1]}')
