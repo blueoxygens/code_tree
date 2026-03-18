849615e97db72b094b62c0550fd670d55e6d8ffc
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2026-03-18 20:42:38 +0900

    Title: 각 정점까지의 최단 경로, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260318/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
+++ 260318/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
@@ -5,37 +5,27 @@ k = int(input())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
 
 # Please write your code here.
+graph = [[] for _ in range(n+1)]
+for f, t, v in edges:
+    graph[f].append((v,t))
+    graph[t].append((v,f))
+
 distance = [math.inf] * (n+1)
 distance[k] = 0
-visited = set()
-visited.add(k)
-heap = []
-for f, t, v in edges:
-    heapq.heappush(heap, (v,f,t))
+heap = [(0,k)]
 
 while heap:
-    cost, n1, n2 = heapq.heappop(heap)
-    temp = []
-    while n1 not in visited and n2 not in visited:
-        if not heap:
-            break
-        temp.append((cost,n1,n2))
-        cost, n1, n2 = heapq.heappop(heap)
-    if n1 in visited:
-        new_cost = distance[n1] + cost
-        if distance[n2] > new_cost:
-            distance[n2] = new_cost
-        visited.add(n2)
-    if n2 in visited:
-        new_cost = distance[n2] + cost
-        if distance[n1] > new_cost:
-            distance[n1] = new_cost
-        visited.add(n1)
-    for t in temp:
-        heapq.heappush(heap, t)
+    cost, node = heapq.heappop(heap)
+
+    if cost > distance[node]:
+        continue
+    
+    for next_cost, next_node in graph[node]:
+        new_cost = cost + next_cost
+        if new_cost < distance[next_node]:
+            distance[next_node] = new_cost
+            heapq.heappush(heap,(new_cost, next_node))
+
 
 for i in range(1,n+1):
-    print(distance[i]) if distance[i] != math.inf else print(-1)
-
-        
-
+    print(distance[i]) if distance[i] < math.inf else -1
