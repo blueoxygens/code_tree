a1e168f9d56df2e058baebb5bd5c7ca7a6b6ed67
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2026-04-12 21:31:19 +0900

    Title: 집합의 원소, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260412/집합의 원소/elements-of-a-set.py
@@ -0,0 +1,30 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+commands = [tuple(map(int,input().split(" "))) for _ in range(m)]
+parents = [i for i in range(n+1)]
+
+def find(x):
+    if parents[x] != x:
+        return find(parents[x])
+    return x
+
+def union(x,y):
+
+    x = find(x)
+    y = find(y)
+
+    if x < y:
+        parents[y] = x
+    else:
+        parents[x] = y
+
+
+for c, x, y in commands:
+    if c == 0:
+        union(x,y)
+    else:
+        if parents[x] == parents[y]:
+            print(1)
+        else:
+            print(0)
