9a6d62f5f8f6a592dd7cde1f7c748ab9373322c5
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2026-04-14 02:21:29 +0900

    Title: 경로의 적합성 판단 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260414/경로의 적합성 판단 2/determining-the-suitability-of-the-route-2.py
@@ -0,0 +1,32 @@
+n, m, k = map(int, input().split())
+
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+path = list(map(int, input().split()))
+
+# Please write your code here.
+
+parent = [i for i in range(n+1)]
+
+def find (a):
+    if parent[a] != a:
+        parent[a] = find(parent[a])
+    return parent[a]
+
+def union (a,b):
+    x = find(a)
+    y = find(b)
+
+    if x > y:
+        parent[a] = y
+    else:
+        parent[b] = x
+
+for e1, e2 in edges:
+    union(e1,e2)
+
+for i in range(k-1):
+    if find(path[i]) != find(path[i+1]):
+        print(0)
+        break
+else:
+    print(1)
