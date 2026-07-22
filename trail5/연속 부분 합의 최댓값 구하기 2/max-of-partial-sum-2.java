4d0ec10fa33cc8806dbd27aab861d74b4bd0a6b1
Kim Hyun Jin <78630493+blueoxygens@users.noreply.github.com>
2026-07-22 14:44:56 +0900

    [Passed] 연속 부분 합의 최댓값 구하기 2 (432ms, 19MB)

---
+++ trail5/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        // 시간 복잡도 O(n)
+        int[] arr = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int left = 0;
+        int sum = 0;
+        int ans = Integer.MIN_VALUE;
+        for(int right = 0; right<n; right++){
+            if(sum < 0){
+                left = right;
+                sum = 0;
+            }
+            sum += arr[right];
+            ans = Math.max(ans,sum);
+        }
+        System.out.println(ans);
+    }
+}
